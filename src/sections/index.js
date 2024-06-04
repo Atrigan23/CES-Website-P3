c3c125e1ffa1ca1a33c2be9e7521648fb3dda1d6
Atrigan23 <pethonir@gmail.com>
2024-06-04 10:42:51 +0200

    Configure React app for deployment to GitHub Pages

---
+++ src/sections/index.js
@@ -0,0 +1,26 @@
+import Navbar from "./Navbar";
+import AboutUs from "./AboutUs";
+import Services from "../sections/Services";
+import OurServices from "../sections/OurServices";
+import Managers from "./Managers";
+import Subcribe from "../sections/Subcribe";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Expierence from "./Expierence";
+
+
+
+
+export {
+
+  Navbar,
+  AboutUs,
+  Services,
+  OurServices,
+  Managers,
+  Subcribe,
+  Footer,
+  Hero,
+  Expierence,
+
+};
